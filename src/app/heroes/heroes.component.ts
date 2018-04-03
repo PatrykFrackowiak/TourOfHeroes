9584dacd8e3284235470fdc3144dca4aaa6ec53e
Patryk Frackowiak <patryk.frackowiak90@gmail.com>
2018-04-03 08:58:35 +0200

    Message component added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../Hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import {MessageService} from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,13 +13,19 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
-    this.heroes = HEROES;
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
+    this.messageService.add('Chosen Hero: ' + hero.name);
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
